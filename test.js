e08fd8ebbf4eba4b8259875dbc27cc6587c2c0dc
Isabel Seidl <isabelseidl@Isabels-MacBook-Pro.local>
2018-09-07 11:36:01 +0200

    Implemented Vue for Congress 113

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
-(function(){
-    
-  
-    
-    
-})()
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+})
 
